b6b3dc6b62330014bff404ee490144a82d3b3121
aksaddleback <142381968+aksaddleback@users.noreply.github.com>
2025-02-20 04:19:15 +0000

    feb 19 codes

---
+++ S04/struct0.cpp
@@ -0,0 +1,13 @@
+#include<iostream> 
+#include<fstream> 
+using namespace std; 
+
+struct Employee { 
+    int id; 
+    string name; 
+    int salary; 
+};
+
+int main() { 
+    Employee e1 = {1001, "Alex", 60000};
+}
